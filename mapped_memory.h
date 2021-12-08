de46224c644eaaf11b4b2c356a1131c21d062c72
Yigit Gunduc <ygunduc@gmail.com>
2021-12-08 16:30:00 +0300

    source and header files move to the .src/

--- mapped_memory.h
+++
@@ -1,7 +0,0 @@
-#ifndef MAPPED_MEMORY_H
-#define MAPPED_MEMORY_H
-
-char *map_memory_for_read(const char *file_name);
-char *map_memory_for_write(const char *file_name);
-
-#endif // MAPPED_MEMORY_H
